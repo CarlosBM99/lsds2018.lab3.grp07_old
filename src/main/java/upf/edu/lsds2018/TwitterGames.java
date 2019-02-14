ccc886bc7d29acc2ccb05bf5b2d2b4b21b12fd3b
Luca <tukaaa@gmail.com>
2019-02-14 01:38:13 +0100

    Indents

--- src/main/java/upf/edu/lsds2018/TwitterGames.java
+++ src/main/java/upf/edu/lsds2018/TwitterGames.java
@@ -1,15 +1,6 @@
 package upf.edu.lsds2018;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
 import upf.edu.lsds2018.model.SimplifiedTweet;
-
-import java.io.Serializable;
-import java.util.Objects;
-import java.util.Optional;
 
 public class TwitterGames {
 
@@ -26,24 +17,23 @@ public class TwitterGames {
 
             // 2.1
             long numberOfTweets = // do something on tweets
-                    System.out.println("Total tweets: " + numberOfTweets);
+            System.out.println("Total tweets: " + numberOfTweets);
 
             // 2.2
             long numberOfOriginalTweets = // do something on tweets
-                    // Transform long into float
-                    System.out.println("Original tweets ratio: " + /*	value goes here */);
+            System.out.println("Original tweets ratio: " + /*	value goes here */);
 
             // 2.3
             long uniqueUsers = // do something on tweets
 
-                    System.out.println("Number of distinct users: " + uniqueUsers);
+            System.out.println("Number of distinct users: " + uniqueUsers);
 
             // Top 10 users by followers
             JavaRDD<SimplifiedTweet> byFollowers = // do something on tweets
-                    JavaRDD<SimplifiedTweet> top10Followers = // do something on the previous step
+            JavaRDD<SimplifiedTweet> top10Followers = // do something on the previous step
 
-                            // Store here both outputs (the top10 and the overall list)
-                            byFollowers.saveAsTextFile(outputDir + "/byFollowers");
+            // Store here both outputs (the top10 and the overall list)
+            byFollowers.saveAsTextFile(outputDir + "/byFollowers");
             top10Followers.saveAsTextFile(outputDir + "/top10Followers");
         }
     }
--- src/main/java/upf/edu/lsds2018/model/SimplifiedTweet.java
+++ src/main/java/upf/edu/lsds2018/model/SimplifiedTweet.java
@@ -7,15 +7,15 @@ public class SimplifiedTweet implements Serializable {
 
     private static JsonParser parser = new JsonParser();
 
-    private final long tweetId;			// the id of the tweet ('id')
-    private final String text;  		// the content of the tweet ('text')
-    private final long userId;			// the user id ('user->id')
-    private final String userName;		// the user name ('user'->'name')
-    private final long followersCount;	// the number of followers ('user'->'followers_count')
-    private final boolean isRetweeted;	// is it a retweet? (the object 'retweeted_status' exists?)
-    private final Long retweetedUserId; // [if retweeted] ('retweeted_status'->'user'->'id')
+    private final long tweetId;		        // the id of the tweet ('id')
+    private final String text;  	        // the content of the tweet ('text')
+    private final long userId;		        // the user id ('user->id')
+    private final String userName;	        // the user name ('user'->'name')
+    private final long followersCount;	    // the number of followers ('user'->'followers_count')
+    private final boolean isRetweeted;	    // is it a retweet? (the object 'retweeted_status' exists?)
+    private final Long retweetedUserId;     // [if retweeted] ('retweeted_status'->'user'->'id')
     private final String retweetedUserName; // [if retweeted] ('retweeted_status'->'user'->'name')
-    private final long timestampMs;		// seconds from epoch ('timestamp_ms')
+    private final long timestampMs;		    // seconds from epoch ('timestamp_ms')
 
     public SimplifiedTweet(long tweetId, String text, long userId, String userName,
                            long followersCount, boolean isRetweeted,
